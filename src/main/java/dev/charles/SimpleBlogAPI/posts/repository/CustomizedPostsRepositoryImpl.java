801cb1ae79551ee8b81c83da4bf3c963f2e83807
Charles <rhcharles0@gmail.com>
2025-11-27 22:24:46 +0900

    perf: replace slow LIKE queries with Full-Text Search

--- src/main/java/dev/charles/SimpleBlogAPI/posts/repository/CustomizedPostsRepositoryImpl.java
+++ src/main/java/dev/charles/SimpleBlogAPI/posts/repository/CustomizedPostsRepositoryImpl.java
@@ -1,12 +1,13 @@
-package dev.charles.SimpleService.posts.repository;
+package dev.charles.SimpleBlogAPI.posts.repository;
 
 import com.querydsl.core.types.Projections;
 import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.core.types.dsl.Expressions;
 import com.querydsl.jpa.JPQLQuery;
 import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
-import dev.charles.SimpleService.posts.domain.Posts;
-import dev.charles.SimpleService.posts.dto.PostDto;
+import dev.charles.SimpleBlogAPI.posts.domain.Posts;
+import dev.charles.SimpleBlogAPI.posts.dto.PostDto;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
@@ -19,8 +20,8 @@ import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
 
-import static dev.charles.SimpleService.posts.domain.QPosts.posts;
-import static dev.charles.SimpleService.users.domain.QUsers.users;
+import static dev.charles.SimpleBlogAPI.posts.domain.QPosts.posts;
+import static dev.charles.SimpleBlogAPI.users.domain.QUsers.users;
 
 
 public class CustomizedPostsRepositoryImpl extends QuerydslRepositorySupport implements CustomizedPostsRepository {
@@ -30,10 +31,6 @@ public class CustomizedPostsRepositoryImpl extends QuerydslRepositorySupport imp
         this.queryFactory = queryFactory;
     }
 
-    private BooleanExpression likeIgnoreCase(String keyword) {
-        if(Optional.ofNullable(keyword).isEmpty()) return null;
-        return posts.title.likeIgnoreCase("%" + keyword + "%");
-    }
 
     @Override
     public Page<PostDto> findAllByKeyword(boolean isSearchMode, String keyword, Pageable pageable) {
@@ -41,17 +38,14 @@ public class CustomizedPostsRepositoryImpl extends QuerydslRepositorySupport imp
         JPAQuery<Long> idQuery = queryFactory
                 .select(posts.id)
                 .from(posts)
-                .where(
-                        likeIgnoreCase(keyword)
-                )
+                .where(searchText(keyword))
                 .orderBy(posts.id.desc());
-        JPQLQuery<Long> paginationId = querydsl().applyPagination(pageable, idQuery);
-        List<Long> ids = paginationId.fetch();
 
+        JPQLQuery<Long> paginationIdQuery = querydsl().applyPagination(pageable, idQuery);
+        List<Long> ids = paginationIdQuery.fetch();
         if(ids.isEmpty()){
             return new PageImpl<>(new ArrayList<>(), PageRequest.of(0,10), 0);
         }
-
         JPAQuery<PostDto> query = queryFactory.select(Projections.fields(PostDto.class,
                                         posts.title,
                                         posts.content))
@@ -66,8 +60,11 @@ public class CustomizedPostsRepositoryImpl extends QuerydslRepositorySupport imp
             int fixedPageCount = 10 * pageable.getPageSize();
             return new PageImpl<>(content, pageable, fixedPageCount);
         }
-
-        long totalCount = paginationId.fetchCount();
+        Long totalCount = queryFactory
+                .select(posts.id.count())
+                .from(posts)
+                .where(searchText(keyword))
+                .fetchOne();
         return new PageImpl<>(content, pageable, totalCount);
     }
 
@@ -78,8 +75,8 @@ public class CustomizedPostsRepositoryImpl extends QuerydslRepositorySupport imp
                 .from(posts)
                 .join(posts.createdBy, users)
                 .where(
-                        likeIgnoreCase(keyword),
-                        users.email.eq(email)
+                        searchText(keyword),
+                        posts.createdBy.email.eq(email)
                 )
                 .orderBy(posts.id.desc());
         JPQLQuery<Long> paginationId = querydsl().applyPagination(pageable, idQuery);
@@ -93,7 +90,6 @@ public class CustomizedPostsRepositoryImpl extends QuerydslRepositorySupport imp
                         posts.title,
                         posts.content))
                 .from(posts)
-                .join(posts.createdBy, users)
                 .where(
                        posts.id.in(ids)
                 )
@@ -105,8 +101,18 @@ public class CustomizedPostsRepositoryImpl extends QuerydslRepositorySupport imp
             int fixedPageCount = 10 * pageable.getPageSize();
             return new PageImpl<>(content, pageable, fixedPageCount);
         }
-        long totalCount = paginationId.fetchCount();
+
+        Long totalCount = queryFactory
+                .select(posts.id.count())
+                .from(posts)
+                .join(posts.createdBy, users)
+                .where(
+                        searchText(keyword),
+                        posts.createdBy.email.eq(email)
+                )
+                .fetchOne();
         return new PageImpl<>(content, pageable, totalCount);
+
 
     }
 
@@ -114,5 +120,15 @@ public class CustomizedPostsRepositoryImpl extends QuerydslRepositorySupport imp
         return Objects.requireNonNull(getQuerydsl());
     }
 
+    private BooleanExpression searchText(String keyword) {
+        String lang = "english";
+        if(Optional.ofNullable(keyword).isEmpty()) return null;
+        return Expressions.booleanTemplate(
+                "search_text({0} ,{1}, {2})",
+                posts.post_tsv,
+                Expressions.constant(lang),
+                Expressions.constant(keyword)
+        );
+    }
 
 }
